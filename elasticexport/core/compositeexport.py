1c5c49b35404e2658a91cbd66a896c903b975de8
Bernard Costa <bernard.costa@virail.com>
2022-01-21 12:05:04 +0100

    saving to file and auto run sh

--- elasticexport/core/compositeexport.py
+++ elasticexport/core/compositeexport.py
@@ -6,38 +6,33 @@ import logging
 import sys
 log = logging.getLogger(__name__)
 
-# def get_keys(dictionary):
-#     for key, value in dictionary.items():
-#         yield key
-#         if type(value) is dict:
-#             yield from get_keys(value)
-#
-#
-# def key_exists(keys):
-#     if 'composite' not in keys:
-#         raise KeyError('"Composite" key must be in query.')
-
 
 if __name__ == "__main__":
 
-    logging.basicConfig(filename=f'std.log', level=logging.DEBUG, filemode='w', format='[%(asctime)s-%(levelname)s] %(name)s: %(message)s')
-    log.debug(f'Starting export')
-    log.debug(sys.argv[1])
-    log.debug(len(sys.argv))
+    logging.basicConfig(filename=f'std.log', level=logging.INFO, filemode='w', format='[%(asctime)s-%(levelname)s] %(name)s: %(message)s')
+    log.info(f'Starting export')
+    log.info(sys.argv[1])
+    log.info(len(sys.argv))
     try:
-        es = Elasticsearch([f'{os.environ["LOCALHOST"]}:5601'])
-        log.debug("Connected to elasticsearch server")
+        es = Elasticsearch([os.environ["PFBISERVER"]])
+        log.info("Connected to elasticsearch server")
+        log.info(str(sys.argv[1]))
         with open(str(sys.argv[1]), encoding='utf-8') as f:
             query = json.loads(f.read())
-
+            log.info('Reading request file')
+        # print(query)
+        # query = '{"query": {"match_all": {}}}'
         # key_exists([key for key in get_keys(query)])
-        while True:
-            res = es.search(index=os.environ['INDEX'], body=query)
-            print(res['aggregations']['two']['buckets'])
-            if "after_key" not in res['aggregations']['two']:
-                break
+        with open(sys.argv[2], 'w') as out:
+            while True:
+                res = es.search(index=str(os.environ['INDEX']), body=query)
+                # print(res)
+                # print(r)
+                out.write(str(res['aggregations']['two']['buckets']))
+                if "after_key" not in res['aggregations']['two']:
+                    break
 
-            after_dict = {"urls":res['aggregations']['two']['after_key']['urls']}
-            query['aggs']['two']["composite"]["after"] = after_dict
+                after_dict = {"urls":res['aggregations']['two']['after_key']['urls']}
+                query['aggs']['two']["composite"]["after"] = after_dict
     except Exception as e:
-        log.error(e)
+        log.info(e)
