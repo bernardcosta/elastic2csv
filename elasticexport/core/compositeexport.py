4999057807133fdbdd457a0dbcda2a0a29b0972d
Bernard Costa <costa.ber95@gmail.com>
2022-01-21 20:08:18 +0100

    dumping to separate organised directories

--- elasticexport/core/compositeexport.py
+++ elasticexport/core/compositeexport.py
@@ -4,7 +4,19 @@ import os
 from datetime import datetime
 import logging
 import sys
+import traceback
 log = logging.getLogger(__name__)
+
+
+def mkdir(rel_path):
+    # Check whether the specified path exists or not
+    if not os.path.exists(rel_path):
+      # Create a new directory because it does not exist
+      os.makedirs(rel_path)
+      log.info(f'New Directory Created: {rel_path}')
+    else:
+        log.info(f'Path already exists. Will overwrite output')
+
 
 
 if __name__ == "__main__":
@@ -14,25 +26,26 @@ if __name__ == "__main__":
     log.info(sys.argv[1])
     log.info(len(sys.argv))
     try:
+        output_path = os.path.join("out",f"{sys.argv[2]}-{str(datetime.now()).replace(' ','')}")
+        mkdir(output_path)
+
         es = Elasticsearch([os.environ["PFBISERVER"]])
         log.info("Connected to elasticsearch server")
         log.info(str(sys.argv[1]))
         with open(str(sys.argv[1]), encoding='utf-8') as f:
             query = json.loads(f.read())
             log.info('Reading request file')
-        # print(query)
-        # query = '{"query": {"match_all": {}}}'
-        # key_exists([key for key in get_keys(query)])
-        with open(sys.argv[2], 'w') as out:
+
+        with open(os.path.join(output_path,'dump.json'), 'a+') as out:
             while True:
                 res = es.search(index=str(os.environ['INDEX']), body=query)
-                # print(res)
-                # print(r)
+                
                 out.write(str(res['aggregations']['two']['buckets']))
+                out.write("\n")
                 if "after_key" not in res['aggregations']['two']:
                     break
 
                 after_dict = {"urls":res['aggregations']['two']['after_key']['urls']}
                 query['aggs']['two']["composite"]["after"] = after_dict
     except Exception as e:
-        log.info(e)
+        log.exception()
