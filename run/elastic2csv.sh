1180da38ac7ef1caf75538db2db6ab938830840a
Bernard Costa <costa.ber95@gmail.com>
2022-02-04 11:15:54 +0100

    es hostname shortcuts from .env file

--- run/elastic2csv.sh
+++ run/elastic2csv.sh
@@ -1,62 +1,16 @@
 #!/bin/bash
-function main(){
-
-  QUERY_REQUEST=""
-  ESDOMAIN=""
-  INDEX=""
-  OUTPUT="./out/exportedData.csv"
-
-  OPTIND=1
-  # Resetting OPTIND is necessary if getopts was used previously in the script.
-  # It is a good idea to make OPTIND local if you process options in a function.
-  while getopts "hq:o:i:d:s:" opt; do
-         case $opt in
-             h) usage
-                exit 0 ;;
-             q)  QUERY_REQUEST=$OPTARG ;;
-             o)  OUTPUT=$OPTARG ;;
-             i)  INDEX=$OPTARG ;;
-             d)  ESDOMAIN=$OPTARG ;;
-             s)  SERVER=$OPTARG ;;
-             *)  usage >&2
-                 exit 1 ;;
-         esac
-     done
-
-
-  check_mandatory_fields;
-
-  shift "$((OPTIND-1))"   # Discard the options and sentinel --
-
-  # Port forward remote elasticsearch server
-  ssh -f -N -q -L ${ESDOMAIN} ${SERVER}
-
-
-  # run python script to extract data from elasticsearch
-  #python3 main.py ${QUERY_REQUEST} ${OUTPUT}
-
-
-  # run python script to extract data from elasticsearch
-  #python3 main.py $2 $3
-
-  # extract useful values from json to csv
-  #cat tmp_dump.json | jq -r '["url","unique sessions","revenue", "total sessions"], (.[] | [.key.urls,.one.value,.three.value,.doc_count]) | @csv' > $3
-  #cat tmp_dump.json | jq -r '.[] | [.key.split,.three.value,.one.value,.five.value,.six.value,.doc_count] | @csv' > $3
-
-  #python3 ./core/cleanLPs.py
-
-  # remove all unnecesary double quotes
-  #sed -i '' 's/"//g' $3
-
-
-  # Get all rows with <= 5 commas
-  #grep -xE '([^,]*,){0,$3}[^,]*' $3 > tmp.csv
-  #rm $3
-  #mv tmp.csv $3
-
+domain(){
+  if [ "$1" = "bi" ]; then
+    echo ${BI};
+  elif [ "$1" = "search" ]; then
+    echo ${SEARCH};
+  elif [ "$1" = "ARCHIVE" ]; then
+    echo ${ARCHIVE}
+  else echo $1;
+  fi
 }
 
-function usage(){
+usage(){
 cat << EOF
 elastic2csv - automated process for exporting elastic queries to csv files
 
@@ -71,7 +25,7 @@ Usage: ${0##*/} [options] [-q QUERY] <json file> [-d DOMAIN] <hostname>
 EOF
 }
 
-function check_mandatory_fields(){
+check_mandatory_fields(){
   if [[ -z $QUERY_REQUEST  ||  -z $ESDOMAIN ]]
   then
     echo "ERROR: -q <query> and -d <hostname> are mandatory arguments. See usage: \n";
@@ -79,7 +33,58 @@ function check_mandatory_fields(){
     exit 1;
   fi
 }
+
 # export .env files
 export $(grep -v '^#' ../.env | xargs)
 
-main "$@"; exit
+QUERY_REQUEST=""
+ESDOMAIN=""
+OUTPUT="./out/exportedData.csv"
+
+OPTIND=1
+# Resetting OPTIND is necessary if getopts was used previously in the script.
+# It is a good idea to make OPTIND local if you process options in a function.
+while getopts "hq:o:i:d:s:" opt; do
+       case $opt in
+           h) usage
+              exit 0 ;;
+           q)  QUERY_REQUEST=$OPTARG ;;
+           o)  OUTPUT=$OPTARG ;;
+           i)  INDEX=$OPTARG ;;
+           d)  ESDOMAIN=$OPTARG ;;
+           s)  SERVER=$OPTARG ;;
+           *)  usage >&2
+               exit 1 ;;
+       esac
+   done
+
+
+check_mandatory_fields;
+
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+# Port forward remote elasticsearch server
+ssh -f -N -q -L "9201:"$( domain ${ESDOMAIN} ) ${SERVER}
+
+
+# run python script to extract data from elasticsearch
+#python3 main.py ${QUERY_REQUEST} ${OUTPUT}
+
+
+# run python script to extract data from elasticsearch
+#python3 main.py $2 $3
+
+# extract useful values from json to csv
+#cat tmp_dump.json | jq -r '["url","unique sessions","revenue", "total sessions"], (.[] | [.key.urls,.one.value,.three.value,.doc_count]) | @csv' > $3
+#cat tmp_dump.json | jq -r '.[] | [.key.split,.three.value,.one.value,.five.value,.six.value,.doc_count] | @csv' > $3
+
+#python3 ./core/cleanLPs.py
+
+# remove all unnecesary double quotes
+#sed -i '' 's/"//g' $3
+
+
+# Get all rows with <= 5 commas
+#grep -xE '([^,]*,){0,$3}[^,]*' $3 > tmp.csv
+#rm $3
+#mv tmp.csv $3
