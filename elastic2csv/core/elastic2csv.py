1dc8f9eb54da739eeaff1f3e0aacdea584f814d1
Bernard Costa <costa.ber95@gmail.com>
2022-02-12 19:26:00 +0100

    Implementation of conneciton and search

--- elastic2csv/core/elastic2csv.py
+++ elastic2csv/core/elastic2csv.py
@@ -1,12 +1,75 @@
-class Elastic2csv
+from elasticsearch import Elasticsearch
+import progressbar
+import logging
+log = logging.getLogger(__name__)
+import json
+import os
 
-def __init__(self):
-    pass
+class Elastic2csv:
+
+    def __init__(self, arguments, scroll_time='1m', timeout=180):
+        self.args = arguments
+        self.scroll = scroll_time
+        self.scroll_ids = []
+        self.timeout = timeout
+        self.connection = None
+        self.query = None
 
 
-def connect(self):
-    es = elasticsearch.Elasticsearch(self.opts.url, timeout=CONNECTION_TIMEOUT, http_auth=self.opts.auth,
-                                     verify_certs=self.opts.verify_certs, ca_certs=self.opts.ca_certs,
-                                     client_cert=self.opts.client_cert, client_key=self.opts.client_key)
-    es.cluster.health()
-    self.es_conn = es
+    #TODO: retry connections
+    def connect(self):
+        es = Elasticsearch(self.args.url, timeout=self.timeout)
+        self.connection = es
+
+    def search(self):
+        self.load_request_file()
+        res = self.connection.search(index=str(self.args.index)+"-*", query=self.query["query"], aggs=self.query["aggs"], scroll=self.scroll)
+        num_results = res['hits']['total']['value']
+        log.info(f'Total Results {num_results}')
+        scroll_id = res["_scroll_id"]
+
+        total_hits = 0
+
+        widgets = ['Run query ',
+                       progressbar.Bar(left='[', marker='#', right=']'),
+                       progressbar.FormatLabel(' [%(value)i/%(max)i] ['),
+                       progressbar.Percentage(),
+                       progressbar.FormatLabel('] [%(elapsed)s] ['),
+                       progressbar.ETA(), '] [',
+                       progressbar.FileTransferSpeed(unit='docs'), ']'
+                       ]
+        bar = progressbar.ProgressBar(widgets=widgets, maxval=num_results).start()
+
+        while total_hits != num_results:
+            total_hits += 1
+            if res['_scroll_id'] not in self.scroll_ids:
+                self.scroll_ids.append(res["_scroll_id"])
+
+            if not res['hits']['hits']:
+                break
+
+            for hit in res['hits']['hits']:
+                 # out.write(json.dumps(res['aggregations'][split_key]['buckets']))
+                 # out.write("\n")
+                 bar.update(total_lines)
+
+
+
+                 res = self.connection.scroll(scroll=self.scroll, scroll_id=res["_scroll_id"])
+
+
+    def clean_scroll_ids(self):
+        try:
+            if self.scroll_ids:
+                self.es_conn.clear_scroll(body=','.join(self.scroll_ids))
+            else:
+                log.info("no scroll ids")
+        except:
+            pass
+
+
+    def load_request_file(self):
+        with open(str(self.args.request_file), encoding='utf-8') as f:
+            request = json.loads(f.read())
+            log.info(f'Loading {self.args.request_file} - Size {os.path.getsize(self.args.request_file) / 1000}Kb')
+            self.query = request
--- elastic2csv/main.py
+++ elastic2csv/main.py
@@ -1,29 +1,48 @@
 from core import compositeexport as ce
+from core import elastic2csv
 from dotenv import load_dotenv
 import logging
 log = logging.getLogger(__name__)
 import sys
 import os, shutil
 import traceback
+import argparse
 
 
 if __name__ == "__main__":
     load_dotenv()
     logging.basicConfig(level=logging.INFO, format='[%(asctime)s-%(levelname)s] %(name)s: %(message)s')
     log.info(f'Starting export')
-    log.info(sys.argv[1])
-    log.info(sys.argv[2])
 
+
+    args = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    args.add_argument('-r', '--request-file', type=str, required=True, help='Json file containing query.')
+    args.add_argument('-u', '--url', type=str, default='http://localhost:9200',help='Host server url with default: %(default)s.')
+    args.add_argument('-i', '--index', type=str, required=True, help='Index pattern name example "logstash". "-*" will be added.')
+
+    #
+    # try:
+    #     ce.mkdir("out")
+    #     #TODO: improve call to local elasticsearch from sh file to python to module
+    #     es = ce.connect_elasticsearch()
+    #
+    #     req = ce.load_request(sys.argv[1])
+    #     log.info('dumping response')
+    #     output_file = ce.search_and_export(es, req, "out", sys.argv[2])
+    #     # make a copy to root directory for further pipeline manipulation
+    #     shutil.copy(output_file, 'tmp_dump.json')
+    #
+    # except Exception as e:
+    #     log.exception("compositeexport.py")
+    es = elastic2csv.Elastic2csv(args.parse_args())
     try:
-        ce.mkdir("out")
-        #TODO: improve call to local elasticsearch from sh file to python to module
-        es = ce.connect_elasticsearch()
 
-        req = ce.load_request(sys.argv[1])
-        log.info('dumping response')
-        output_file = ce.search_and_export(es, req, "out", sys.argv[2])
-        # make a copy to root directory for further pipeline manipulation
-        shutil.copy(output_file, 'tmp_dump.json')
+        es.connect()
+        es.search()
 
     except Exception as e:
-        log.exception("compositeexport.py")
+        log.exception("main.py")
+
+    finally:
+        log.info("closing scrolls")
+        es.clean_scroll_ids()
