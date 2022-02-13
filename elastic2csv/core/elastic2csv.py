9ce9ce92faf50554ecc3bea21eb44f739f103e53
Bernard Costa <costa.ber95@gmail.com>
2022-02-13 18:24:53 +0100

    Added saving as csv with hardcoded headers

--- elastic2csv/core/elastic2csv.py
+++ elastic2csv/core/elastic2csv.py
@@ -5,7 +5,8 @@ log = logging.getLogger(__name__)
 import json
 import os
 from datetime import datetime
-
+import csv
+import utils
 
 
 class Elastic2csv:
@@ -23,10 +24,8 @@ class Elastic2csv:
 
     def port_forward(self):
         os.system("kill $( ps aux | grep '[9]201:' | awk '{print $2}' )")
-        log.info("Using port 9201 to forward remote server to local.")
         os.system(f'ssh -f -N -q -L "9201:{self.args.url}" {self.args.server_username}@{self.args.server_host}')
         self.args.url="http://localhost:9201"
-
 
 
     #TODO: retry connections
@@ -35,12 +34,8 @@ class Elastic2csv:
         self.connection = es
 
     def search(self):
-        self.load_request_file()
-        # res = self.connection.search(index=str(self.args.index)+"-*", query=self.query["query"], aggs=self.query["aggs"])
-        # num_results = res['hits']['total']['value']
-        # log.info(f'Total Results {num_results}')
-        # scroll_id = res["_scroll_id"]
         total_hits = 0
+        self.load_request_file()
 
         # widgets = ['Run query ',
         #                progressbar.Bar(left='[', marker='#', right=']'),
@@ -51,27 +46,42 @@ class Elastic2csv:
         #                progressbar.FileTransferSpeed(unit='docs'), ']'
         #                ]
         # bar = progressbar.ProgressBar(widgets=widgets, maxval=num_results).start()
+
         outfile = os.path.join(self.args.out_dir,f'dump{str(datetime.now()).replace(" ","")}.json')
         split_key = self.find_key(self.query)[-2]
 
         with open(outfile, 'a+') as out:
             count = 1
+            out.write("[")
             while True:
+
                 res = self.connection.search(index=str(self.args.index)+"-*", query=self.query["query"], aggs=self.query["aggs"])
                 for hit in res['aggregations'][split_key]['buckets']:
+                    if total_hits > 0:
+                        out.write(",\n")
                     total_hits += 1
-
                     out.write(json.dumps(hit))
-                    out.write("\n")
 
                 if "after_key" not in res['aggregations'][split_key]:
+                    out.write("]")
                     break
 
                 after_dict = {"split":res['aggregations'][split_key]['after_key']['split']}
-                log.info(f'p{count} - {count * 700}: {after_dict}')
                 self.query['aggs'][split_key]["composite"]["after"] = after_dict
                 count = count + 1
+                out.write("]")
+                break
 
+        data = json.load(open(outfile, 'r'))
+        data = utils.flatten_json_list(data)
+        print(data)
+
+        with open("output.csv","w") as f:  # python 2: open("output.csv","wb")
+
+            columns = ["key.split","doc_count","three.value","one.value","five.value","six.value"] # quick hack
+            cw = csv.DictWriter(f, columns, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, extrasaction='ignore')
+            cw.writeheader()
+            cw.writerows(data)
 
     def load_request_file(self):
         with open(str(self.args.request_file), encoding='utf-8') as f:
